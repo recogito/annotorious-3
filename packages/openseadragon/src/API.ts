fd440310f16ec3d8f6b39dc7a3f4c1118900b025
Rainer Simon <rainer@rainersimon.io>
2022-10-26 19:30:54 +0200

    Minor tweaks + added loadAnnotations API method

--- packages/openseadragon/src/API.ts
+++ packages/openseadragon/src/API.ts
@@ -1,20 +1,34 @@
 import type OpenSeadragon from 'openseadragon';
 import type { APIOptions } from './APIOptions';
 import { Store } from '@annotorious/core';
-import { parseW3C } from '@annotorious/formats';
+import { parseW3C, type WebAnnotation } from '@annotorious/formats';
 import OSDPixiImageAnnotationLayer from './pixi/OSDPixiImageAnnotationLayer.svelte';
+import OSDSVGDrawingLayer from './drawing/OSDSVGDrawingLayer.svelte';
 
 export class API {
-  anno: OSDPixiImageAnnotationLayer;
+  annotationLayer: OSDPixiImageAnnotationLayer;
+
+  drawingLayer: OSDSVGDrawingLayer;
 
   constructor(viewer: OpenSeadragon.Viewer, opts: APIOptions) {
-    this.anno = new OSDPixiImageAnnotationLayer({
+    this.annotationLayer = new OSDPixiImageAnnotationLayer({
+      target: viewer.container,
+      props: { viewer }
+    });
+
+    this.drawingLayer = new OSDSVGDrawingLayer({
       target: viewer.container,
       props: { viewer }
     });
   }
 
-  setAnnotations = (annotations: any[]) => {
+  loadAnnotations = (url: string) => fetch(url)
+    .then(response => response.json()).then((annotations: WebAnnotation[]) => {
+      this.setAnnotations(annotations);
+      return annotations;
+    });
+
+  setAnnotations = (annotations: WebAnnotation[]) => {
     const { parsed } = parseW3C(annotations);
     Store.set(parsed);
   };
